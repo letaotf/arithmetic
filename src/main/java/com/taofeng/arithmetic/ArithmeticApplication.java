f89c29796067cc3cfd51e5d8dbef0e1e301e31af
乐陶 <letao@maihaoche.com>
2019-09-29 16:26:58 +0800

    设置应用的端口号

--- src/main/java/com/taofeng/arithmetic/ArithmeticApplication.java
+++ src/main/java/com/taofeng/arithmetic/ArithmeticApplication.java
@@ -2,12 +2,20 @@ package com.taofeng.arithmetic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ArithmeticApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArithmeticApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "我是乐陶";
 	}
 
 }
